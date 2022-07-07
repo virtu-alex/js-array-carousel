cdec82c7f5761a64435d9d8fa04ae63a02c5d5c9
virtu-alex <alessio.cordari@gmail.com>
2022-07-07 19:12:46 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -1,40 +1,83 @@
-/*
-Creato un array contenente una lista di cinque immagini tra quelle fornite, creare un carosello ispirandoci alle foto in allegato.
-(non è necessario che la grafica sia IDENTICA alla traccia, non bloccatevi su questo, date un aspetto decente e concentriamoci sulla logica.
-Cerchiamo di lavorare seguendo i seguenti step in ordine:
+// ## Consegna
+// Dato un array di oggetti letterali con:
+// - url dell’immagine
+// - titolo
+// - descrizione
+// Creare un carosello ispirandosi alla foto allegata. Potete anche usare come base il carosello dell'esercizio precedente
+//// ## Milstone 0:
+// Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l’immagine grande in modo da poter stilare lo slider.
+//// ## Milestone 1:
+// Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
+// Al click dell’utente sulle frecce verso sinistra o destra, l’immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+//// ## Milestone 2:
+// Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l’utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l’ultima e viceversa per l’ultima miniatura se l’utente clicca la freccia verso sinistra.
 
-MILESTONE 1
+// ---
+//// ## BONUS 1:
+// Aggiungere le thumbnails (sottoforma di miniatura) ed al click attivare l’immagine corrispondente.
 
-Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider; avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
+//// ## BONUS 2:
+// Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva dovrà cambiare alla successiva.
 
-MILESTONE 2
+// ## BONUS 3:
+// Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
 
-Adesso rimuoviamo tutto il markup statico e inseriamo le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for. Possiamo concatenare una stringa con un template literal oppure utilizzare gli altri metodi di manipolazione del DOM che abbiamo visto insieme. Al termine di questa fase ci ritroveremo con lo stesso slider, ma costruito dinamicamente attraverso JavaScript.
-*/
 
-// CREO UN ARRAY
-const sources = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
+const images = [
+    {
+        url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
+        title: 'Svezia',
+        description:
+            'La Svezia è un paese scandinavo che comprende migliaia di isole lungo la costa e laghi nell entroterra, oltre a vaste foreste boreali e rilievi glaciali',
+    },
+    {
+        url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
+        title: 'Perù',
+        description:
+            'Il Perù è una nazione sudamericana che ospita una parte della Foresta Amazzonica e Machu Picchu, l antica città inca situata sulla catena delle Ande.',
+    },
+    {
+        url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
+        title: 'Chile',
+        description:
+            'Il Cile è un paese lungo e stretto che si estende lungo il confine occidentale del Sud America, con più di 6000 km di costa affacciata sull Oceano Pacifico.',
+    },
+    {
+        url: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
+        title: 'Argentina',
+        description:
+            'L Argentina è uno stato sudamericano molto esteso con un territorio che comprende la cordigliera delle Ande, laghi glaciali e le pianure della Pampa, il tradizionale terreno di pascolo dei famosi bovini da carne.',
+    },
+    {
+        url: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop',
+        title: 'Colombia',
+        description:
+            'La Colombia è un paese che si trova sulla punta nord del Sud America.',
+    },
+];
+
 // prendo elemento carousel dal DOM
 const carousel = document.getElementById('carousel');
 const thumbnailContainer = document.getElementById('thumbnail');
 
+let index = 1;
 // per ogni elemento nell array... 
-for (let i = 0; i < sources.length; i++) {
+for (let i = 0; i < images.length; i++) {
     // creo un elemento con tag img
     const img = document.createElement('img');
     // assegno all 'attributo src dell'elemento i-esimo dell'array
-    img.setAttribute('src', sources[i]);
+    img.setAttribute('src', images[i].url);
     // inserisco nell'elemento carousel l'immagine appena creata
     carousel.append(img);
     const thumbImage = document.createElement('img');
-    thumbImage.setAttribute('src', sources[i]);
+    thumbImage.setAttribute('src', images[i].url);
     thumbImage.classList.add('opacity-50');
     thumbnailContainer.append(thumbImage);
 }
 
 // AZIONE 1.2 RECUPERO TRAMITE QUERYSELECTOR LE IMMAGINI
 const myImages = document.querySelectorAll('#gallery img');
-
+// AZIONE 1.2 RECUPERO TRAMITE QUERYSELECTOR LE IMMAGINI PER IL THUMBNAIL
 const myThumbsImg = document.querySelectorAll('#thumbnail img');
 
 // AZIONE 2 CREO UNA VARIABILE D'APPOGGIO CHE VALUTI LA POSIZIONE ATTUALE
@@ -51,27 +94,11 @@ Al click dell'utente sulle frecce, l'immagine attiva cambia e diventa visibile n
 // AZIONE 4 RECUPERO I BOTTONI DALL'HTML
 const btnNext = document.getElementById('next');
 const btnPrev = document.getElementById('previous');
+const titleEl = document.getElementById('title')
+const descriptionEl = document.getElementById('description')
 
 // AZIONE 5 EVENT LISTNER BOTTONE NEXT
-btnNext.addEventListener('click', function () {
-    // RIMUOVO LA CLASSE ACTIVE
-    myImages[currentActiveIndex].classList.remove('active');
-    //RIMUOVO LA CLASSE SCALE
-    myThumbsImg[currentActiveIndex].classList.remove('scale');
-    //AGGIUNGO LA CLASSE OPACITY
-    myThumbsImg[currentActiveIndex].classList.add('opacity-50');
-    // AUMENTA L'INDICE DI 1
-    currentActiveIndex++;
-    // CONTROLLA SE L'INDICE E' MAGGIORE DELLA LUNGEZZA DELL'ARRAY PERCHE' NON DEVE SUPERARE L'ULTIMO ELEMENTO
-    if (currentActiveIndex > sources.length - 1) {
-        // SE E' MAGGIORE LO REIMPOSTO AL PRIMO ELEMENTO DELL'ARRAY
-        currentActiveIndex = 0;
-    }
-    //ALL'IMMAGINE SUCCESSIVA APPLICO (PERCHE' HO AUMENTATO L'INDICE DI 1)    
-    myImages[currentActiveIndex].classList.add('active');
-    myThumbsImg[currentActiveIndex].classList.add('scale');
-    myThumbsImg[currentActiveIndex].classList.remove('opacity-50');
-});
+btnNext.addEventListener('click', nextImage);
 
 // AZIONE 6 EVENT LISTNER BOTTONE PREVIOUS
 btnPrev.addEventListener('click', function () {
@@ -83,25 +110,42 @@ btnPrev.addEventListener('click', function () {
     // CONTROLLA SE L'INDICE NON SIA MINORE DI 0 (IN QUESTO MODO NON ASSUME VALORI NEGATIVI)
     if (currentActiveIndex < 0) {
         // SE E' MINORE DI 0 LO REIMPOSTO ALL' ULTIMO ELEMENTO DELL'ARRAY
-        currentActiveIndex = sources.length - 1;
+        currentActiveIndex = images.length - 1;
     }
+    updateText(images[currentActiveIndex].title, images[currentActiveIndex].description);
     myImages[currentActiveIndex].classList.add('active');
     myThumbsImg[currentActiveIndex].classList.add('scale');
     myThumbsImg[currentActiveIndex].classList.remove('opacity-50');
 });
 
-/*BONUS 1:
+updateText(images[currentActiveIndex].title, images[currentActiveIndex].description);
 
-Aggiungere il ciclo infinito del carosello. Ovvero se l' immagine attiva è la prima e l'utente clicca la freccia per andare indietro, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso avanti, deve attivarsi la prima immgine.*/
+function updateText(title, description) {
+    titleEl.innerText = title;
+    descriptionEl.innerText = description;
+}
 
-//BONUS 2:
+const interval = setInterval(nextImage, 3000)
 
-/*Creiamo delle miniature di tutte le immagni, in cui dovrà apparire in evidenza l’immagine equivalente a quella attiva, scegliete liberamente se scurire le altre immagini oppure se evidenziarla semplicemente con un bordo. Tra queste miniature, quella corrispondente all'immagine attiva deve evidenziarsi, scegliete voi l'effetto estetico, potete colorarla diversamente rispetto alle altre o aggiungere un semplice bordo.
-Prima di partire a scrivere codice:
-Non lasciamoci spaventare dalla complessità apparente dell'esercizio, ma analizziamo prima, come abbiamo fatto sempre, cosa ci potrebbe aspettare. Abbiamo completato ormai da qualche settimana la sessione HTML e CSS, se non ci ricordiamo qualcosa andiamo pure a riguardare alcuni argomenti. Non dedichiamo però al ripasso più di una mezz'ora, così da non perdere di vista il focus dell'esercizio.
+function nextImage() {
 
-Consigli del giorno:
-
-1. Costruiamo del carosello una versione statica contenente un'immagine grande con del testo ben posizionato e una miniatura. Di questa versione statica al momento opportuno commenteremo (oscureremo) alcuni elementi per poterli riprodurre dinamicamente in js. Potremo quindi usarli come "template".
-2. Scriviamo sempre prima per punti il nostro algoritmo in italiano per capire cosa vogliamo fare
-3. Al momento giusto (starà a voi capire quale) rispondete a questa domanda: "Quanti cicli servono?"*/
+    // RIMUOVO LA CLASSE ACTIVE
+    myImages[currentActiveIndex].classList.remove('active');
+    //RIMUOVO LA CLASSE SCALE
+    myThumbsImg[currentActiveIndex].classList.remove('scale');
+    //AGGIUNGO LA CLASSE OPACITY
+    myThumbsImg[currentActiveIndex].classList.add('opacity-50');
+    // AUMENTA L'INDICE DI 1
+    currentActiveIndex++;
+    
+    // CONTROLLA SE L'INDICE E' MAGGIORE DELLA LUNGEZZA DELL'ARRAY PERCHE' NON DEVE SUPERARE L'ULTIMO ELEMENTO
+    if (currentActiveIndex > images.length - 1) {
+        // SE E' MAGGIORE LO REIMPOSTO AL PRIMO ELEMENTO DELL'ARRAY
+        currentActiveIndex = 0;
+    }
+    updateText(images[currentActiveIndex].title, images[currentActiveIndex].description);
+    //ALL'IMMAGINE SUCCESSIVA APPLICO (PERCHE' HO AUMENTATO L'INDICE DI 1)    
+    myImages[currentActiveIndex].classList.add('active');
+    myThumbsImg[currentActiveIndex].classList.add('scale');
+    myThumbsImg[currentActiveIndex].classList.remove('opacity-50');
+}
